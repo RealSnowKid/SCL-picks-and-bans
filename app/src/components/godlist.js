85c1584e4878be199a95b15b85d44c706414d065
Kevin Bevers <kevin.bevers@hotmail.com>
2021-05-25 01:31:40 +0200

    some final touches to the god select component

--- app/src/components/godlist.js
+++ app/src/components/godlist.js
@@ -8,9 +8,22 @@ export default function GodList({pickedGods, pickNumber}) {
     const [filter, setFilter] = useState("");
     const [selectedGod, setSelectedGod] = useState("");
 
-    const pickGod = (godName) => {
+    const selectGod = (godName) => {
         setSelectedGod(godName);
-        console.log(godName);
+        // make call to function in parent here with godName and pickNumber for the hover.
+    };
+    //link this function to button 
+    const lockGod = (e) => {
+        e.preventDefault();
+
+        if(selectedGod == "" || selectedGod == null)
+        {
+            alert("please select a god before trying to lock in.");
+        }
+        else {
+            //use selectedGod and make call to function in parent with selectedGod and pickNumber.
+        }
+        
     };
 
     function isNotPicked(god) {
@@ -26,6 +39,7 @@ export default function GodList({pickedGods, pickNumber}) {
     const handleFilter = (e) => {
         setFilter(e.target.value);
         setFilteredGods(gods.filter(x => x.name.toLowerCase().includes(e.target.value.toLowerCase())));
+        setSelectedGod("");
     };
 
     //only runs on first render
@@ -44,13 +58,13 @@ export default function GodList({pickedGods, pickNumber}) {
 
         return (
             <div className="flex flex-col h-full mt-5">
-            <input className="w-3/4 mx-auto my-1" type="text" placeholder="Search god..." value={filter} onChange={handleFilter} />    
+            <input className="w-3/4 mx-auto my-1" type="text" placeholder="Search god..." value={filter} onChange={handleFilter} />  <button className="w-1/2 mx-auto bg-white rounded border border-black hover:bg-gray-300" type="submit" onClick={lockGod}>Lock in god</button>
             <div class="container mx-auto px-4 overflow-scroll h-4/5 mt-2 border-solid border-4 border-black">                 
             <section class="py-8 px-4">
               <div class="flex flex-wrap -mx-4 -mb-8">
                   {filteredGods?.length > 0 ? 
                   filteredGods.map((g, index) => (
-                    <div class={`md:w-28 px-4 mb-8`} onClick={(e) => {pickGod(g.name.toLowerCase())}} ><img className={`rounded shadow-md ${g.name.toLowerCase() == selectedGod ? "border-solid border-4 border-blue-500" : ""}`} src={g.godIcon_URL} alt="" /></div>
+                    <div class={`md:w-28 px-4 mb-8`} onClick={(e) => {selectGod(g.name.toLowerCase())}} ><img className={`rounded shadow-md ${g.name.toLowerCase() == selectedGod ? "border-solid border-4 border-blue-500" : ""}`} src={g.godIcon_URL} alt="" /></div>
                   )) : "" }
                
               </div>
