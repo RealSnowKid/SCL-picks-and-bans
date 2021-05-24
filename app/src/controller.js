09f6482e52b7a10cff295f572daa4bb92210c82c
Kevin Bevers <kevin.bevers@hotmail.com>
2021-05-24 20:39:30 +0200

    fixed typo

--- app/src/controller.js
+++ app/src/controller.js
@@ -8,6 +8,10 @@ export default function Controller() {
     // eslint-disable-next-line
     const [step, setStep] = useState(0);
 
+    function updateStep(newValue) {
+        setStep(newValue);
+    };
+
     let json = [
         {
             matchScoreT1: 0,
@@ -124,20 +128,20 @@ export default function Controller() {
                 <div className="w-1/2 h-full bg-snl-yellow">
                     <form className="flex flex-col h-20" onSubmit={handleFormSubmit}>
                         <h1 className="text-2xl mx-auto">General Information</h1>
-                        <input className="w-3/4 mx-auto my-1" type="text" placeholder="Score (Fromat: 0-0)" />
+                        <input className="w-3/4 mx-auto my-1" type="text" placeholder="Score (Format: 0-0)" />
                         <input className="w-3/4 mx-auto my-1" type="text" placeholder="Patch (Format: 8.5)" />
                         <div className="w-full flex mt-2">
                             <div className="w-1/2 flex flex-col mx-1">
                                 <h1 className="text-2xl mx-auto">Team 1</h1>
                                 <input className="w-full mx-auto my-1" type="text" placeholder="Team 1 Name" />
                                 <input className="w-full mx-auto my-1" type="text" placeholder="Team 1 Logo URL" />
-                                <input className="w-full mx-auto my-1" type="text" placeholder="Team 1 League Score (Fromat: 0-0)" />
+                                <input className="w-full mx-auto my-1" type="text" placeholder="Team 1 League Score (Format: 0-0)" />
                             </div>
                             <div className="w-1/2 flex flex-col mx-1">
                                 <h1 className="text-2xl mx-auto">Team 2</h1>
                                 <input className="w-full mx-auto my-1" type="text" placeholder="Team 2 Name" />
                                 <input className="w-full mx-auto my-1" type="text" placeholder="Team 2 Logo URL" />
-                                <input className="w-full mx-auto my-1" type="text" placeholder="Team 2 League Score (Fromat: 0-0)" />
+                                <input className="w-full mx-auto my-1" type="text" placeholder="Team 2 League Score (Format: 0-0)" />
                             </div>
                         </div>
                         <button className="w-1/2 mx-auto bg-white rounded border border-black hover:bg-gray-300" type="submit">Send</button>
@@ -145,7 +149,7 @@ export default function Controller() {
                 </div>
                 <div className="w-1/2 h-full bg-snl-dark">
                     <div className="w-full flex">
-                        <MatchInfo message={things} />
+                        <MatchInfo message={things} updateFunction={updateStep} />
                     </div>
                 </div>
             </div>
