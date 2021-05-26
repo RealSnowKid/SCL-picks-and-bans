48c3673725214bbc5d1912c721110cac7ae54528
Kevin Bevers <kevin.bevers@hotmail.com>
2021-05-27 01:27:13 +0200

    added debug

--- app/src/components/godPick.js
+++ app/src/components/godPick.js
@@ -17,11 +17,13 @@ function GodPick({ God, Hover, Role, Active }) {
         setTransition(true);
             // setTimeout(() => {
             //     setTransition(false);
+            console.log("image done");
             // }, 600);
     };
 
     useEffect(() => {
         imageLoading();
+        console.log("image loading");
     }, [God]);
 
     useEffect(() => {
@@ -84,7 +86,7 @@ function GodPick({ God, Hover, Role, Active }) {
 
     return (
         <div className={varContainerClassName}>
-            <img src={url} onLoad={() => setTransition(false)} className={`${varClassName} ${transition ? "transitioning-src" : "easin-src"}`} alt={"pick-" + role}/>
+            <img src={url} onLoad={() => {setTransition(false); console.log("image loading");}} className={`${varClassName} ${transition ? "transitioning-src" : "easin-src"}`} alt={"pick-" + role}/>
             {/* <h2 className="text-white relative top-7">Nike</h2> */}
             <div className={varClassNameHover}></div>
             { active ? <div className={varBlinkerClassName}></div> : <> </>}
