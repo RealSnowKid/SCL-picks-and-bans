9e03a7b038574d04828318a7183355d3b36ef5d6
Kevin Bevers <kevin.bevers@hotmail.com>
2021-05-27 12:37:12 +0200

    new godPick. should not bug out anymore ;)

--- app/src/components/godPick.js
+++ app/src/components/godPick.js
@@ -1,29 +1,22 @@
 import React, { useState, useEffect } from 'react';
 
 function GodPick({ God, Hover, Role, Active }) {
-    // const [god, setGod] = useState("");
-    // const [god2, setGod2] = useState("");
-    // const [hover, setHover] = useState(true);
-    // const [role, setRole] = useState("");
-    // const [active, setActive] = useState("");
     const [url, setUrl] = useState("");
     const [godClass, setGodClass] = useState("");
     const [hoverClass, setHoverClass] = useState("");
     const [containerClass, setContainerClass] = useState("");
     const [blinkerClass, setBlinkerClass] = useState("");
-    //let url;
-    // let varClassName;
-    // let varClassNameHover;
-    // let varContainerClassName;
-    // let varBlinkerClassName;
-    const [transition, setTransition] = useState(true);
+    const [transition, setTransition] = useState(false);
 
     const imageLoading = () => {
         setTransition(true);
-            setTimeout(() => {
+    };
+
+    const imageLoaded = () => {
+         setTimeout(() => {
                 setTransition(false);
             console.log(`image done ${God}`);
-            }, 600);
+            }, 400);
     };
 
     useEffect(() => {
@@ -32,47 +25,44 @@ function GodPick({ God, Hover, Role, Active }) {
     }, [url]);
 
     useEffect(() => {
-        let god = "";
-        let god2 = "";
+        console.log("selected god: " + God);
+        if (God?.length > 0) {
+            if (God === "ah muzen cab") {
+                setUrl("https://webcdn.hirezstudios.com/smite/god-skins/" + "ah_muzen_cab" + "_standard-" + "ah-muzen-cab" + ".jpg");
+                setGodClass("ah-muzen-cab");
+            }
+            else if (God === "da ji") {
+                const url = "https://webcdn.hirezstudios.com/smite/god-skins/" + "da_ji" + "_standard-" + "daji" + ".jpg";
+                setUrl(url);
+                setGodClass("daji");
+            }
+            else if (God === "chang'e") {
+                const url = "https://webcdn.hirezstudios.com/smite/god-skins/" + "change" + "_standard-" + "change" + ".jpg";
+                setUrl(url);
+                setGodClass("change");
+            }
+            else {
+                const god1 = God;
+                const god2 = God;
+                const url = "https://webcdn.hirezstudios.com/smite/god-skins/" + god1.replace(" ", "_") + "_standard-" + god2.replace(" ", "-") + ".jpg";
+                setUrl(url);
+                setGodClass(god2.replace(/[ ]{1,}/g, "-"));
+            }
 
-        if (God.toString().toLowerCase() !== "") {
-            if (God.toString().toLowerCase().match(/([A-z]+[ ]{1,})+[A-z]+/g)) {
-                if (God.toString().toLowerCase() === "ah muzen cab") {
-                    god = "ah_muzen_cab";
-                    god2 = "ah-muzen-cab";
-                }
-                else if (God.toString().toLowerCase() === "da ji") {
-                    god = God.toString().toLowerCase().replace(/[ ]{1,}/g, "_");
-                    god2 = "daji";
-                }
-                else {
-                    god = God.toString().toLowerCase().replace(/[ ]{1,}/g, "_");
-                    god2 = God.toString().toLowerCase().replace(/[ ]{1,}/g, "-");
-                }
-            }
-            else if (god === "chang'e") {
-                god = "change";
-                god2 = "change";
-            }
-            
-            setUrl("https://webcdn.hirezstudios.com/smite/god-skins/" + god + "_standard-" + god2 + ".jpg");
-            setGodClass(god2);
-            console.log("god as background image set");
+            console.log("Url: " + url);
 
             if (Hover === true) {
                 setHoverClass("hover-" + Role.toString() + " opacity-70");
-                setContainerClass("h-32 w-80 mx-2 mt-2 overflow-hidden border-2 border-white");
+                setContainerClass("h-32 w-80 mx-2 mt-2 bg-black overflow-hidden border-2 border-white");
             }
             else {
                 setHoverClass("hover-" + Role.toString() + " opacity-70 locked");
-                setContainerClass("h-32 w-80 mx-2 mt-2 overflow-hidden border-2 border-white");
+                setContainerClass("h-32 w-80 mx-2 mt-2 bg-black overflow-hidden border-2 border-white");
             }
-    
         }
         else {
             setUrl("https://i.imgur.com/Bx0iFjJ.png");
             setGodClass("bg-contain m-small-logo");
-            console.log("default background image set");
         }
     }, [God, Hover]);
 
@@ -93,9 +83,9 @@ function GodPick({ God, Hover, Role, Active }) {
 
     return (
         <div className={containerClass}>
-            <img src={url} className={`${godClass} ${transition ? "transitioning-src" : "easin-src"}`} alt={"pick-" + Role.toString()}/>
+            <img src={url} onLoad={imageLoaded} className={`${godClass} ${transition ? "transitioning-src" : "easin-src"}`} alt={"pick-" + Role.toString()}/>
             {/* <h2 className="text-white relative top-7">Nike</h2> */}
-            <div className={hoverClass}></div>
+            <div className={transition ? "" : hoverClass}></div>
             { Active ? <div className={blinkerClass}></div> : <> </>}
         </div>
     );
